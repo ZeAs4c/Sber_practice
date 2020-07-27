8ca3ac0d6f9d022bc2e681de61e7e239cb28f7eb
alexk <alexk@DESKTOP-Q509TJ6>
2020-07-28 00:31:52 +0300

    18 task complete

--- SBERTEHD2/src/com/sbrf/demo/task19.java
+++ SBERTEHD2/src/com/sbrf/demo/task19.java
@@ -5,52 +5,7 @@ import java.util.Scanner;
 public class task19 {
     public static void main(String[] args) {
         /*Задача 19 */
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите значение число: ");
-        int n = scanner.nextInt();
-        String a,b,c,d,f,q,s,w;
-        if(n<=10)
-        {
-            switch (n){
-                case 1:
-                    a="I";
-                    break;
-                case 2:
-                    a="II";
-                    break;
-                case 3:
-                    a="III";
-                    break;
-                case 4:
-                    a="IV";
-                    break;
-                case 5:
-                    a="V";
-                    break;
-                case 6:
-                    a="VI";
-                    break;
-                case 7:
-                    a="VII";
-                    break;
-                case 8:
-                    a="VII";
-                    break;
-                case 9:
-                    a="IX";
-                    break;
-            }
-            b="X";
-            c="L";
-            d="C";
-            if(n<=100&&10<=n)
-            {
-                switch (n){
-                    case 1 -> 12:
-                        
 
-                }
-            }
         }
-    }
 }
+
