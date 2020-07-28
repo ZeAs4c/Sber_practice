4fc0350eb78ab969fb163dddcb183b23c411785d
alexk <alexk@DESKTOP-Q509TJ6>
2020-07-28 07:34:42 +0300

    19 task complete

--- SBERTEHD2/src/com/sbrf/demo/task17.java
+++ SBERTEHD2/src/com/sbrf/demo/task17.java
@@ -1,8 +1,37 @@
 package com.sbrf.demo;
 
+import java.util.Scanner;
+
 public class task17 {
     public static void main(String[] args) {
         /*Задача 17 */
+        Scanner scanner = new Scanner(System.in);
+        int[][] arr = new int[6][7];
+        for (int i = 0; i < 6; ++i) {
+            for (int j = 0; j < 7; ++j) {
+                arr[i][j] = (int) (Math.random() * (9 - 0) + 0);
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < 6; ++i)
+            for (int j = 6; j >= 1; --j) {
+            if (arr[i][j] > arr[i][j - 1]) {
+                int tmp = arr[i][j];
+                arr[i][j] = arr[i][j - 1];
+                arr[i][j - 1] = tmp;
+            }
+        }
+
+        for (int i = 0; i < 6; ++i) {
+            for (int j = 0; j < 7; ++j) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
 
--- SBERTEHD2/src/com/sbrf/demo/task19.java
+++ SBERTEHD2/src/com/sbrf/demo/task19.java
@@ -3,9 +3,42 @@ package com.sbrf.demo;
 import java.util.Scanner;
 
 public class task19 {
-    public static void main(String[] args) {
-        /*Задача 19 */
+    /*Задача 19 */
+    enum Numeral {
+        I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90), C(100), CD(400), D(500), CM(900), M(1000);
+        int weight;
 
+        Numeral(int weight) {
+            this.weight = weight;
+        }
+    };
+
+    public static String roman(long n) {
+
+        if( n <= 0) {
+            System.out.println("Число должно быть больше нуля!");
+        }
+
+        StringBuilder buf = new StringBuilder();
+
+        final Numeral[] values = Numeral.values();
+        for (int i = values.length - 1; i >= 0; i--) {
+            while (n >= values[i].weight) {
+                buf.append(values[i]);
+                n -= values[i].weight;
+            }
+        }
+        return buf.toString();
+    }
+
+    public static void test(long n) {
+        System.out.println(n + " = " + roman(n));
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите арабское число, которое нужно перевести в римское число: ");
+        int a = scanner.nextInt();
+        test(a);
         }
 }
 
